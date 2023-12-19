dc214e65f9ad4158ae4256a1a687a8e2b0056570
Dominic Udousoro <dominicudousoro@gmail.com>
2023-12-18 20:40:20 -0800

    Updating Files

--- findschlapp/backend/app.py
+++ findschlapp/backend/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, jsonify
-from flask_mongoengine import MongoEngine
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'institutions_db',
-    'host': 'mongodb://localhost:27017/institutions_db',
-}
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/institutions'
 
-mongo = MongoEngine(app)
+mongo = PyMongo(app)
 
-class Institution(mongo.Document):
-    name = mongo.StringField()
-    state = mongo.StringField()
-    ownership = mongo.StringField()
+class Institution:
+    def __init__(self, name, state, ownership):
+        self.name = name
+        self.state = state
+        self.ownership = ownership
 
 # Endpoint to parse JSON and insert into the database
 @app.route('/upload', methods=['POST'])
@@ -21,8 +19,17 @@ def upload_json():
         json_data = request.get_json()
 
         for institution_data in json_data.get('institutions', []):
-            institution = Institution(**institution_data)
-            institution.save()
+            institution = Institution(
+                name=institution_data.get('name'),
+                state=institution_data.get('state'),
+                ownership=institution_data.get('ownership')
+            )
+            # Insert into MongoDB
+            mongo.db.institutions.insert_one({
+                'name': institution.name,
+                'state': institution.state,
+                'ownership': institution.ownership
+            })
 
         return jsonify({'message': 'Data uploaded successfully'})
     except Exception as e:
@@ -33,9 +40,10 @@ def upload_json():
 def search_institutions():
     try:
         state = request.args.get('state')
-        institutions = Institution.objects(state=state)
+        # Query MongoDB
+        institutions = mongo.db.institutions.find({'state': state})
 
-        result = [{'name': inst.name, 'state': inst.state, 'ownership': inst.ownership} for inst in institutions]
+        result = [{'name': inst['name'], 'state': inst['state'], 'ownership': inst['ownership']} for inst in institutions]
         return jsonify({'institutions': result})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
